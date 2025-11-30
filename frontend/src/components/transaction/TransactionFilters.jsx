9bf842e3e93f29d7e4ba0ee6feecffb326d751cf
aryanpuri <aryan.puri@mail.utoronto.ca>
2025-11-30 14:45:56 -0500

    Pagination Works

--- frontend/src/components/transaction/TransactionFilters.jsx
+++ frontend/src/components/transaction/TransactionFilters.jsx
@@ -246,9 +246,7 @@ export function TransactionFilters({
                         </label>
                         <input
                             type="number"
-                            min="1"
                             value={currentPage}
-                            placeholder="1"
                             onChange={(e) => setCurrentPage(parseInt(e.target.value, 10))}
                             className="w-full px-3 py-2 border border-slate-200 rounded-lg
   text-sm focus:outline-none focus:ring-2 focus:ring-emerald-500"
